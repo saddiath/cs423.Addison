8b6ba88515526f4befd14b083a0051a127b567ea
Paul Addison <seanaddison@Seans-MacBook-Pro.local>
2023-09-26 14:37:58 -0500

    Added Assignment 5 req.

--- ArtMuseum/Assets/PaintingTele.cs
+++ ArtMuseum/Assets/PaintingTele.cs
@@ -8,11 +8,24 @@ public class PaintingTele : MonoBehaviour
     [Header("Keybinds")]
     public KeyCode interaction = KeyCode.E;
 
+    public Transform painting1;
+    public Transform painting2;
+    public Transform painting3;
+    public Transform painting4;
+
+
+    private Transform pos;
+
+    public List<Transform> list = new List<Transform>();
+
     public Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        list.Add(painting1);
+        list.Add(painting2);
+        list.Add(painting3);
+        list.Add(painting4);
     }
 
     // Update is called once per frame
@@ -20,7 +33,8 @@ public class PaintingTele : MonoBehaviour
     {
         if (Input.GetKey(interaction))
         {
-            player.position = new Vector3(2, 2, 2);
+            pos = list[Random.Range(0, 3)];
+            player.position = new Vector3(pos.position.x, pos.position.y, pos.position.z);
         }
     }
 }
